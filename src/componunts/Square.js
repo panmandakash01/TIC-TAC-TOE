153f3aceaaf25818a7974876098d3ed6d55c879b
panmandakash01 <panmandak01@gmail.com>
2022-07-06 15:54:59 +0530

    app created sucessfully

--- src/componunts/Square.js
+++ src/componunts/Square.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const Square = ({ value, onClick, isWinningSquare }) => {
+const Square = ({ value, onClick, isWinningSquare}) => {
   return (
-    <button type="button" className="square" onClick={onClick} style={{fontWeight:isWinningSquare ? 'bold':'normal'}} >
+    <button 
+    type="button"
+     onClick={onClick}
+     className={`square ${isWinningSquare ? 'winning':''} ${value==='X' ? 'text-green': 'text-orange'}`}
+    
+     >
       {value}
     </button>
   );
